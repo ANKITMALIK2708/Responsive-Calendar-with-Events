d1ee99a1c97b6c7da97caacffa531cddf561bd29
Ankit Malik <ankitmalik2708@gmail.com>
2023-07-13 21:35:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -195,4 +195,123 @@ function prevMonth() {
     year = today.getFullYear();
     initCalendar();
   });
+  dateInput.addEventListener("input", (e) => {
+    dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
+    if (dateInput.value.length === 2) {
+      dateInput.value += "/";
+    }
+    if (dateInput.value.length > 7) {
+      dateInput.value = dateInput.value.slice(0, 7);
+    }
+    if (e.inputType === "deleteContentBackward") {
+      if (dateInput.value.length === 3) {
+        dateInput.value = dateInput.value.slice(0, 2);
+      }
+    }
+  });
   
+  gotoBtn.addEventListener("click", gotoDate);
+  
+  function gotoDate() {
+    console.log("here");
+    const dateArr = dateInput.value.split("/");
+    if (dateArr.length === 2) {
+      if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+        month = dateArr[0] - 1;
+        year = dateArr[1];
+        initCalendar();
+        return;
+      }
+    }
+    alert("Invalid Date");
+  }
+  
+  //function get active day day name and date and update eventday eventdate
+  function getActiveDay(date) {
+    const day = new Date(year, month, date);
+    const dayName = day.toString().split(" ")[0];
+    eventDay.innerHTML = dayName;
+    eventDate.innerHTML = date + " " + months[month] + " " + year;
+  }
+  
+  //function update events when a day is active
+  function updateEvents(date) {
+    let events = "";
+    eventsArr.forEach((event) => {
+      if (
+        date === event.day &&
+        month + 1 === event.month &&
+        year === event.year
+      ) {
+        event.events.forEach((event) => {
+          events += `<div class="event">
+              <div class="title">
+                <i class="fas fa-circle"></i>
+                <h3 class="event-title">${event.title}</h3>
+              </div>
+              <div class="event-time">
+                <span class="event-time">${event.time}</span>
+              </div>
+          </div>`;
+        });
+      }
+    });
+    if (events === "") {
+      events = `<div class="no-event">
+              <h3>No Events</h3>
+          </div>`;
+    }
+    eventsContainer.innerHTML = events;
+    saveEvents();
+  }
+  
+  //function to add event
+  addEventBtn.addEventListener("click", () => {
+    addEventWrapper.classList.toggle("active");
+  });
+  
+  addEventCloseBtn.addEventListener("click", () => {
+    addEventWrapper.classList.remove("active");
+  });
+  
+  document.addEventListener("click", (e) => {
+    if (e.target !== addEventBtn && !addEventWrapper.contains(e.target)) {
+      addEventWrapper.classList.remove("active");
+    }
+  });
+  
+  //allow 50 chars in eventtitle
+  addEventTitle.addEventListener("input", (e) => {
+    addEventTitle.value = addEventTitle.value.slice(0, 60);
+  });
+  
+  // function defineProperty() {
+  //   var osccred = document.createElement("div");
+  //   osccred.innerHTML =
+  //     "A Project By <a href='https://www.youtube.com/channel/UCiUtBDVaSmMGKxg1HYeK-BQ' target=_blank>Open Source Coding</a>";
+  //   osccred.style.position = "absolute";
+  //   osccred.style.bottom = "0";
+  //   osccred.style.right = "0";
+  //   osccred.style.fontSize = "10px";
+  //   osccred.style.color = "#ccc";
+  //   osccred.style.fontFamily = "sans-serif";
+  //   osccred.style.padding = "5px";
+  //   osccred.style.background = "#fff";
+  //   osccred.style.borderTopLeftRadius = "5px";
+  //   osccred.style.borderBottomRightRadius = "5px";
+  //   osccred.style.boxShadow = "0 0 5px #ccc";
+  //   document.body.appendChild(osccred);
+  // }
+  
+  // defineProperty();
+  
+  // //allow only time in eventtime from and to
+  // addEventFrom.addEventListener("input", (e) => {
+  //   addEventFrom.value = addEventFrom.value.replace(/[^0-9:]/g, "");
+  //   if (addEventFrom.value.length === 2) {
+  //     addEventFrom.value += ":";
+  //   }
+  //   if (addEventFrom.value.length > 5) {
+  //     addEventFrom.value = addEventFrom.value.slice(0, 5);
+  //   }
+  // });
